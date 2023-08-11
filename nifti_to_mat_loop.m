43ea126ce6f42b6f0d9eca61bda75b7fdc41b2dd
James Barry <77990973+James-C-Barry@users.noreply.github.com>
2023-08-11 12:16:44 +0200

    Update nifti_to_mat_loop.m
    This script will loop through a directory to find a specific folder name that contains either .nii.gz or .nii files then convert them to _roi.mat files. The only things to change are the named folder which is the folder containing the images (this should be the same for all subjects if running multiple) and the root directory.

--- nifti_to_mat_loop.m
+++ nifti_to_mat_loop.m
@@ -1,33 +1,80 @@
-% Set the directory containing the NIfTI files
+% This script will loop through a directory to find a specific folder name that contains
+% either .nii.gz or .nii files then convert them to _roi.mat files. The only things to change
+% are the named folder which is the folder containing the images (this should be the same for
+% all subjects if running multiple) and the root directory.
+
+% Set the root directory containing the *NAMED* folders
 rootDir = '/write/your/root/path/here';
 
-% Get a list of folders in the root directory
-folders = dir(rootDir);
-folders = folders([folders.isdir]); % Keep only directories
-folders = folders(~ismember({folders.name}, {'.', '..'})); % Remove '.' and '..' entries
+% Replace *NAMED* with the folder that it will find for each subject
 
-% Loop through each folder
-for i = 1:numel(folders)
-    folderPath = fullfile(rootDir, folders(i).name);
+% Process *NAMED folders and subfolders recursively
+processNIIFolders(rootDir);
+
+function processNIIFolders(folderPath)
+    % Get the name of the current folder
+    [~, folderName, ~] = fileparts(folderPath);
     
-    % Get a list of NIfTI files in the folder
-    niiFiles = dir(fullfile(folderPath, '*.nii'));
+    % Check if the current folder is named "*NAMED*"
+        if strcmp(folderName, 'INPUT FOLDER NAME HERE')
+        % Get a list of NIfTI.gz files in the current folder
+        niiGzFiles = dir(fullfile(folderPath, '*.nii.gz'));
+        
+        % Check if NIfTI.gz files exist, else process NIfTI files
+        if ~isempty(niiGzFiles)
+            % Loop through each NIfTI.gz file
+            for j = 1:numel(niiGzFiles)
+                % Unzip the NIfTI.gz file
+                niiGzFile = fullfile(folderPath, niiGzFiles(j).name);
+                fprintf('Unzipping: %s\n', niiGzFile);
+                gunzip(niiGzFile);
+                
+                % Get the unzipped NIfTI file name
+                [~, filename, ~] = fileparts(niiGzFile);
+                unzippedNiiFile = fullfile(folderPath, filename);
+                
+                % Load and process the NIfTI image
+                processNiftiFile(unzippedNiiFile);
+                
+                % Clean up: remove the unzipped NIfTI file
+                delete(unzippedNiiFile);
+            end
+        else
+            % Get a list of NIfTI files in the current folder
+            niiFiles = dir(fullfile(folderPath, '*.nii'));
+            
+            % Loop through each NIfTI file
+            for j = 1:numel(niiFiles)
+                % Process the NIfTI image
+                niiFile = fullfile(folderPath, niiFiles(j).name);
+                processNiftiFile(niiFile);
+            end
+        end
+    end
+  
+    % Get a list of subdirectories in the current folder
+    subdirs = dir(folderPath);
+    subdirs = subdirs([subdirs.isdir]);
+    subdirs = subdirs(~ismember({subdirs.name}, {'.', '..'}));
     
-    % Loop through each NIfTI file
-    for j = 1:numel(niiFiles)
-        % Load the NIfTI image
-        niiFile = fullfile(folderPath, niiFiles(j).name);
-        V = spm_vol(niiFile);
-        Y = spm_read_vols(V);
-        
-        % Create a new region of interest (ROI) from the NIfTI image
-        roi = maroi_image(struct('vol', V, 'binarize', 0.5));
-        
-        % Create the ROI filename with the same name as the NIfTI file
-        [~, filename, ~] = fileparts(niiFile);
-        roiFile = fullfile(folderPath, [filename '_roi.mat']);
-        
-        % Save the ROI as a separate file
-        save(roiFile, 'roi');
+    % Recursively process subdirectories
+    for i = 1:numel(subdirs)
+        subfolderPath = fullfile(folderPath, subdirs(i).name);
+        processNIIFolders(subfolderPath);
     end
 end
+
+function processNiftiFile(niftiFile)
+    % Load the NIfTI image
+    fprintf('Loading: %s\n', niftiFile);
+    V = spm_vol(niftiFile);
+    Y = spm_read_vols(V);
+    
+    % Create a new region of interest (ROI) from the NIfTI image
+    P = niftiFile;
+    roipath = fileparts(niftiFile);
+    [~, rootn, ~] = fileparts(niftiFile);
+    flags = 'i'; % assuming id image
+    mars_img2rois(P, roipath, rootn, flags);
+end
+
